8f077032a36c920751aeb9adb8c8289a13ddc230
Nasti <anastasiia.bazhenova@gmail.com>
2023-12-07 09:26:43 +0100

    Add input

--- app/(dashboard)/book/[[...slug]]/page.tsx
+++ app/(dashboard)/book/[[...slug]]/page.tsx
@@ -46,7 +46,49 @@ const BookPage = ({
           <option value="ulrich">Family Ulrich</option>
         </select>
 
-        <div className="flex mb-6 flex-wrap">
+        <div className="flex mb-6 xs:flex-col">
+          <div className="flex flex-1 flex-col">
+            <label htmlFor="family-tree-select" className="text-[12px] pl-1">
+              Select a Family Tree
+            </label>
+
+            <select
+              name="family-tree-names"
+              id="family-tree-select"
+              required
+              className="border-2 border-black/5 rounded-lg px-4 py-2 mt-2 appearance-none
+          bg-chevron-down bg-no-repeat bg-right-4 bg-[length:16px_16px]"
+            >
+              <option value="richard">Family Richard</option>
+              <option value="irmgard">Family Irmgard</option>
+              <option value="eberhard">Family Eberhard</option>
+              <option value="albert">Family Albert</option>
+              <option value="ulrich">Family Ulrich</option>
+            </select>
+          </div>
+
+          <div className="flex flex-col sm:mt-0 ml-2 xs:ml-0 xs:mt-4">
+            <label htmlFor="family-tree-select" className="text-[12px] pl-1">
+              How many visitors?
+            </label>
+
+            <select
+              name="visitors"
+              id="visitors"
+              required
+              className="border-2 border-black/5 rounded-lg px-4 py-2 mt-2 appearance-none
+          bg-chevron-down bg-no-repeat bg-right-4 bg-[length:16px_16px]"
+            >
+              {Array.from({ length: 13 }).map((_, index) => (
+                <option key={index.toString()} value={index + 1}>
+                  {index + 1}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+
+        <div className="flex mb-6 xs:flex-col">
           <div className="flex flex-col">
             <label htmlFor="from-date" className="text-[12px] pl-1">
               From
@@ -55,7 +97,7 @@ const BookPage = ({
             <ControlledDateInput type="start" />
           </div>
 
-          <div className="flex flex-col ml-2 mb-4 xxs:ml-0 xxs:mt-4">
+          <div className="flex flex-col ml-2 mb-4 xs:ml-0 xs:mt-4">
             <label htmlFor="to-date" className="text-[12px] pl-1">
               To
             </label>
